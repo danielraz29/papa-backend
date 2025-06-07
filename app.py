caf909c235f83ce728521828e8d70302a0e5dc02
danielraz29 <104888372+danielraz29@users.noreply.github.com>
2025-06-07 12:29:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from routes import adminMentorPage
 from routes import meetingsPage
 from routes import traineesPage
 from routes import mentor_dashboard
+from routes import createMentee
 
 app = FastAPI()
 
@@ -25,7 +26,7 @@ app.include_router(auth.router)
 app.include_router(mentorSwipe.router)
 app.include_router(handleDashboardMentees.router)
 app.include_router(createMentor.router)
-
+app.include_router(createMentee.router)
 app.include_router(adminMentorPage.router)
 app.include_router(meetingsPage.router)
 app.include_router(traineesPage.router)
